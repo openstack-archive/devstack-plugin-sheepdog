289a1f2f53f373da828379ec2ad0b8e9d1563c18
YAMADA Hideki <yamada.hideki@lab.ntt.co.jp>
2017-05-16 09:53:45 +0000

    Make sheep to start with absolute path
    Current devstack run_process needs absolute path.
    
    Change-Id: I719c8fe31465647745c929912e18a6c5f234209d

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -85,9 +85,9 @@ function install_sheepdog {
 # start_sheepdog() - Start running processes, including screen
 function start_sheepdog {
     if [[ ${DISTRO} =~ (trusty) ]]; then
-        run_process sheepdog "sheep -f -o -l 7 -c local -n ${SHEEPDOG_DATA_DIR}"
+        run_process sheepdog "/usr/sbin/sheep -f -o -l 7 -c local -n ${SHEEPDOG_DATA_DIR}"
     elif [[ ${DISTRO} =~ (xenial) ]]; then
-        run_process sheepdog "sheep -l dst=stdout,level=debug,format=server -c local -n ${SHEEPDOG_DATA_DIR}"
+        run_process sheepdog "/usr/sbin/sheep -l dst=stdout,level=debug,format=server -c local -n ${SHEEPDOG_DATA_DIR}"
     else
         exit_distro_not_supported "Sheepdog since your distro doesn't provide (at least) the Firefly release. Please use Ubuntu Trusty or Xenial."
     fi
